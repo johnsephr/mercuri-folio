9b68ff1be204aec949853927fbcee6161e874104
John Ramsey <dxtrdth@Johns-MBP-2.lan1>
2020-01-15 17:13:09 -0600

    commit

--- src/components/Utility/Footer/Footer.js
+++ src/components/Utility/Footer/Footer.js
@@ -1,7 +1,10 @@
 import React, { Fragment } from 'react'
 
+// Components
+import BehanceIcon from '../../Custom/Icons/BehanceIcon'
+
 // MUI
-import { Typography } from '@material-ui/core'
+import { Link, Grid } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
 // Styles
@@ -12,29 +15,71 @@ const useStyles = makeStyles(theme => ({
         margin: '50px 0'
     },
     leftPanel: {
-        width: '50%',
+        display: 'flex',
+        alignItems: 'center',
+        background: 'transparent'
+    },
+    leftPanelText: {
         color: '#72c89e',
+        fontSize: '20px',
+        cursor: 'pointer'
+    },
+    centerPanel: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
         background: 'transparent'
     },
     rightPanel: {
-        width: '50%',
+        display: 'flex',
+        alignItems: 'center',
+        background: 'transparent'
+    },
+    rightPanelText: {
+        width: '100%',
         color: '#72c89e',
         textAlign: 'right',
-        order: 1,
-        background: 'transparent'
+        fontSize: '20px'
     }
 }))
 
 const Footer = props => {
     const classes = useStyles(props)
+
+    const openMail = () => {
+        window.location.href = 'mailto:hello@mercuri.tech'
+    }
+
     return (
         <div className={classes.root}>
-            <Typography variant='h6' className={classes.leftPanel}>
-                hello@mercuri.tech
-            </Typography>
-            <Typography variant='h6' className={classes.rightPanel}>
-                Austin, TX
-            </Typography>
+            <Grid
+                container
+                direction='row'
+                alignContent='center'
+                spacing={1}
+                className={classes.grid}
+            >
+                <Grid item xs={4} className={classes.leftPanel}>
+                    <div className={classes.leftPanelText} onClick={openMail} >
+                        {/* <button target='_blank' rel='noopener'> */}
+                            hello@mercuri.tech
+                        {/* </button> */}
+                    </div>
+                </Grid>
+
+                <Grid item xs={4} className={classes.centerPanel}>
+                    <Link href='https://www.behance.net/mchllgorski' target='_blank' rel='noopener'>
+                        <BehanceIcon />
+                    </Link>
+                </Grid>
+
+                <Grid item xs={4} className={classes.rightPanel}>
+                    <div className={classes.rightPanelText}>
+                        Austin, TX
+                    </div>
+                </Grid>
+            </Grid>
+
         </div>
     )
 }
